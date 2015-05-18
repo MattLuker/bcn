100072ee4aab483d0320d1b13088c3a56f6c534b
Adam Sommer <asommer70@gmail.com>
2015-05-18 07:25:27 -0400

    Have test for pulling User events from Facebook.

--- spec/features/users/facebook_user_spec.rb
+++ spec/features/users/facebook_user_spec.rb
@@ -1,19 +1,13 @@
 require 'rails_helper'
 
 describe 'Signing Up with Facebook' do
-  # before(:each) do
-  #   valid_facebook_login_setup
-  #   visit "auth/facebook/callback"
-  #   request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
-  # end
-  #
-  # it "should set user_id" do
-  #   expect(session[:user_id]).to eq(User.last.id)
-  # end
-  #
-  # it "should redirect to root" do
-  #   expect(response).to redirect_to home_path
-  # end
+  before do
+    Community.create({name: 'BCN Rails',
+                      description: 'BCN Rails Open Source project.',
+                      home_page: 'https://github.com/asommer70/bcn',
+                      color: '#333333'
+                     })
+  end
 
   it 'allows user to sign up via Facebook', :js => true do
     expect(User.count).to eq(0)
@@ -30,9 +24,38 @@ describe 'Signing Up with Facebook' do
     fill_in 'Password:', with: FACEBOOK_CONFIG['password']
     #sleep(10)
     find('#u_0_2').click # 'Log In' button element.
-    sleep 5
 
     expect(User.count).to eq(1)
     expect(page).to have_content("Welcome #{FACEBOOK_CONFIG['first_name']}, you have been registered using Facebook.")
+  end
+
+  it 'creates Events for Facebook user', :js => true do
+    expect(User.count).to eq(0)
+
+    visit '/'
+
+    expect(page).to have_content('Register')
+    click_link 'Login'
+    click_link 'Login with Facebook'
+
+    expect(page).to have_content('Facebook Login')
+
+    fill_in 'Email or Phone:', with: FACEBOOK_CONFIG['username']
+    fill_in 'Password:', with: FACEBOOK_CONFIG['password']
+    #sleep(10)
+    find('#u_0_2').click # 'Log In' button element.
+
+    expect(User.count).to eq(1)
+    expect(page).to have_content("Welcome #{FACEBOOK_CONFIG['first_name']}, you have been registered using Facebook.")
+
+    click_link 'Communities'
+    click_link 'BCN Rails'
+    click_button 'Join Community'
+
+    click_link 'Log Out'
+    click_link 'Login'
+    click_link 'Login with Facebook'
+    
+    expect(page).to have_content('More Facebook Integration')
   end
 end
