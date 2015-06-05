3116157e0942492d5ba0ff2d9ef852c4c920eacd
Adam Sommer <asommer70@gmail.com>
2015-06-05 19:02:16 -0400

    Adjusted the Post list view to make it look good with a web link, and made the Post title and description optional.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,6 @@ class Post < ActiveRecord::Base
   acts_as_paranoid
   dragonfly_accessor :image
 
-  validates :title, presence: true
-  validates :description, presence: true
   validates :start_date, allow_nil: true, format: { with: /.*/, message: 'format must look like: 2015-05-25' }
   validates :start_time, allow_nil: true, format: { with: /.*/, message: 'format must look like 05:05' }
   validates :end_date, allow_nil: true, format: { with: /.*/, message: 'format must look like: 2015-05-25' }
