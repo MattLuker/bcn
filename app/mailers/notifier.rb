f516598ff707daa6a88d229e7e6d78d2a43c3189
Adam Sommer <asommer70@gmail.com>
2015-06-27 13:49:37 -0400

    Adjusted other mailers to check for User first and last name.

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -4,12 +4,12 @@ class Notifier < ApplicationMailer
 
   def password_reset(user)
     @user = user
-    mail(to: "#{user.first_name} #{user.last_name} <#{user.email}>", subject: "Reset Your Password")
+    mail(to: "#{user.first_name if user.first_name} #{user.last_name if user.last_name} <#{user.email}>", subject: "Reset Your Password")
   end
 
   def send_merge(merge_user)
     @user = merge_user
-    mail(to: "#{@user.first_name} #{@user.last_name} <#{@user.email}>", subject: "Merge Account Request")
+    mail(to: "#{@user.first_name if @user.first_name} #{@user.last_name if @user.last_name} <#{@user.email}>", subject: "Merge Account Request")
   end
 
   def send_welcome(user)
