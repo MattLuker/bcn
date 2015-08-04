32d9beced8618f8b0102827e235fd290b3b77fe6
Adam Sommer <asommer70@gmail.com>
2015-08-04 07:19:44 -0400

    Can create, update, view, and index Badges. Need tests though.

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,9 +1,15 @@
 class BadgesController < ApplicationController
+  before_action :find_badge, only: [:show, :edit, :update, :destroy]
+
+  before_filter :require_user, only: [:create, :new, :edit, :update, :destroy]
+  before_filter :check_admin, only: [:create, :new, :edit, :update, :destroy]
+
   def index
     @badges = Badge.all
   end
 
   def show
+    @badge = Badge.find(params[:id])
   end
 
   def new
@@ -11,9 +17,7 @@ class BadgesController < ApplicationController
   end
 
   def create
-    puts "params: #{params}"
-    if @badge = Badge.new(badge_params)
-      puts "@badge: #{@badge.inspect}"
+    if @badge = Badge.create(badge_params)
 
       flash[:success] = 'Badge was successfully created.'
       redirect_to @badge
@@ -23,10 +27,40 @@ class BadgesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
   def update
+    if @badge.update(badge_params)
+      flash[:success] = 'Badge successfully updated.'
+      redirect_to @badge
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @badge.destroy
+      flash[:success] = 'Badge deleted'
+      redirect_to badges_path
+    else
+      flash[:alert] = 'There was a problem deleting the badge.'
+      redirect_to @badge
+    end
   end
 
   private
+    def find_badge
+      @badge = Badge.find(params[:id])
+    end
+
+    def check_admin
+      unless current_user.admin?
+        flash[:error] = 'Page not available.'
+        redirect_to badges_path
+      end
+    end
+
     def badge_params
       params.require(:badge).permit(:name, :rules, :image);
     end
