355a7191b6e9f7f6d60401ad25caa322d984ea2c
Adam Sommer <asommer70@gmail.com>
2015-08-11 06:44:17 -0400

    Updated Community Subscriber notification test to test notify_instant.

--- spec/features/communities/subscribe_spec.rb
+++ spec/features/communities/subscribe_spec.rb
@@ -47,13 +47,15 @@ describe 'Subscribing to Community' do
     expect(community.subscribers.count).to eq(0)
   end
 
-  it 'send email to subscribers when new post is created', :js => true do
+  it 'send email to subscribers when new post is created if notify_instant is true', :js => true do
     sign_in user, password: 'beans'
     create_community
     community = Community.last
 
     click_link 'Log Out'
     sign_in user2, password: 'beans'
+    user2.notify_instant = true
+    user2.save
 
     visit "/communities/#{community.slug}"
     #sleep(5)
@@ -85,5 +87,42 @@ describe 'Subscribing to Community' do
     expect(page).to have_content("Great new post.")
     open_email(user2.email)
     expect(current_email.body).to have_content('Great new post')
+  end
+
+  it 'does not send email to subscribers when new post is created if notify_instant is false', :js => true do
+    sign_in user, password: 'beans'
+    create_community
+    community = Community.last
+
+    click_link 'Log Out'
+    sign_in user2, password: 'beans'
+
+    visit "/communities/#{community.slug}"
+    #sleep(5)
+
+    expect(page).to have_content('Boone Community Network')
+    expect(page).to have_content('We are all part of the Boone community!')
+
+    find('.community-subscribe').click
+    sleep(0.3)
+    expect(community.subscribers[0].user).to eq(user2)
+
+    click_link 'Log Out'
+    sign_in user, password: 'beans'
+
+    visit "/posts"
+    click_link "New Post"
+    expect(page).to have_content("New Post")
+
+    fill_in "Title", with: "My Location Post"
+    page.execute_script("window.post_editor.codemirror.setValue('Great new post.')")
+
+    communites_field = find('input.default')
+    communites_field.set('Boone Community Network')
+    communites_field.native.send_key(:Enter)
+
+    expect {
+      click_button "Save Post"
+    }.to change{ ActionMailer::Base.deliveries.size }.by(0)
   end
 end
