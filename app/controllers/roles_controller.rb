285a721640b4d72e9b85abeb101638b13d671ac4
Adam Sommer <asommer70@gmail.com>
2015-08-18 14:17:04 -0400

    User is notified if Organization membership is denied and Role is destroyed when pending join request is denied.

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -2,19 +2,31 @@ class RolesController < ApplicationController
   def update
     organization = Organization.find_by_slug(params[:organization_id])
     role = Role.find(params[:id])
+    if role_params[:name] == 'remove'
+      if role.destroy
+        Notifier.join_status(role.user, organization, nil).deliver_now
 
-    if role.update(role_params)
-      organization.users << role.user
-      flash[:success] = 'Member status updated.'
-
-      # Send member notification of status update.
-      Notifier.join_status(role.user, organization, role).deliver_now
-
-      redirect_to edit_organization_path(organization)
+        flash[:success] = 'Member status updated.'
+        redirect_to organization_path(organization)
+      else
+        flash[:error] = "There was a problem updating the Member's status."
+        redirect_to edit_organization_path(organization)
+      end
     else
-      flash[:error] = "There was a problem updating the Member's status."
-      redirect_to edit_organization_path(organization)
+      if role.update(role_params)
+        organization.users << role.user
+        flash[:success] = 'Member status updated.'
+
+        # Send member notification of status update.
+        Notifier.join_status(role.user, organization, role).deliver_now
+
+        redirect_to organization_path(organization)
+      else
+        flash[:error] = "There was a problem updating the Member's status."
+        redirect_to edit_organization_path(organization)
+      end
     end
+
   end
 
   private
