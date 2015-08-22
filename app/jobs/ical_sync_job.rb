d19e7aef5282625fa4b5987311a3346b01e2b6f4
Adam Sommer <asommer70@gmail.com>
2015-08-22 15:08:36 -0400

    Adding Organization Location to Posts.

--- app/jobs/ical_sync_job.rb
+++ app/jobs/ical_sync_job.rb
@@ -58,17 +58,12 @@ class IcalSyncJob < ActiveJob::Base
             post.og_title = event.summary
             post.og_image = event.attach[0].to_s
             post.image_url = event.attach[0].to_s
-            #post.create_location({lat: lat, lon: lon}) if lat && lon
+
             unless post.locations.include?(organization.location)
-              puts "post.locations #{post.locations[0].name if post.locations[0]} <-- name"
-              post.locations.push(organization.location.clone)
+              post.locations << organization.location
             end
-            if post.id == 41
-              post.title = 'Balls'
-            end
-            if post.save
-              puts "post.id: #{post.id}, post.title: #{post.title} post.locations.length: #{post.locations.length}"
-            end
+
+            post.save
 
           end
         end
