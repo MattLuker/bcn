4504901c28f752e8078ed51dba7438390bf74e50
Adam Sommer <asommer70@gmail.com>
2015-08-24 06:41:59 -0400

    Working on new Organization join workflow tests.

--- spec/features/organizations/add_user_spec.rb
+++ spec/features/organizations/add_user_spec.rb
@@ -2,13 +2,18 @@ require 'rails_helper'
 
 describe "Adding User to organizations" do
   let(:user) { create(:user) }
+  let(:organization) { Organization.create(name: 'BCN', description: 'BCN Rulez!', color: '#eeeeee', slug: 'bcn')}
 
-  it "is successful with valid content", :js => true do
+  before do
+    Role.create(name: 'admin', user: user)
+    Role.create(name: 'leader', user: user, organization: organization)
+  end
+
+  it "sends join request email to organization leader", :js => true do
     sign_in user, password: 'beans'
     create_organization
     expect(page).to have_content("BCN Rulez!")
     organization = Organization.last
-
     click_link 'Log Out'
 
     cheese = User.create(email: 'cheese@thehoick.com', password: 'beans')
@@ -17,8 +22,8 @@ describe "Adding User to organizations" do
     visit '/organizations/' + organization.slug
     click_button 'Join ' + organization.name
 
-    expect(page).to have_content("You are now part of #{organization.name}.")
-    expect(organization.users.count).to eq(2)
+    open_email(user.email)
+    expect(current_email.body).to have_content("A user has requested membership in #{organization.name}.")
   end
 
   it 'sends notification to the leader when a user joins', :js => true do
@@ -26,7 +31,6 @@ describe "Adding User to organizations" do
     create_organization
     expect(page).to have_content("BCN Rulez!")
     organization = Organization.last
-
     click_link 'Log Out'
 
     cheese = User.create(email: 'cheese@thehoick.com', password: 'beans')
@@ -35,10 +39,23 @@ describe "Adding User to organizations" do
     visit '/organizations/' + organization.slug
     click_button 'Join ' + organization.name
 
-    expect(page).to have_content("You are now part of #{organization.name}.")
+    open_email(user.email)
+    expect(current_email.body).to have_content("A user has requested membership in #{organization.name}.")
+
+    click_link 'Log Out'
+    sign_in user, password: 'beans'
+    visit '/organizations/' + organization.slug
+    find('.edit-organization').click
+
+    click_link 'Members'
+    find('.role_' + user.id.to_s).find(:xpath, 'option[1]').select_option
+    find('.update_' + user.id.to_s).click
+
+
+    expect(page).to have_content("Member status updated.")
     expect(organization.users.count).to eq(2)
 
-    open_email(user.email)
-    expect(current_email.body).to have_content("A new user has joined #{organization.name}.")
+    open_email(cheese.email)
+    expect(current_email.body).to have_content("Your membership in #{organization.nam} has been granted.")
   end
 end
