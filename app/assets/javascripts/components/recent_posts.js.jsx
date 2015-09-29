ef14228c4411d17bc71f497a91abe972e340ed02
Adam Sommer <adam@thehoick.com>
2015-09-29 06:06:59 -0400

    Figured out React iterator key and applied React to Events.

--- app/assets/javascripts/components/recent_posts.js.jsx
+++ app/assets/javascripts/components/recent_posts.js.jsx
@@ -1,7 +1,7 @@
 var RecentPosts = React.createClass({
   render: function() {
     var posts = this.props.data.map(function(posts){
-      return <Post {...posts} />
+      return <Post {...posts} key={posts.id} />
     });
 
     return <ul className="posts no-bullet">
