d9f21dd4b9298b68d7aa6ae1a28ba5077bb86b7f
Adam Sommer <asommer70@gmail.com>
2015-12-10 14:09:04 -0500

    Added box-shadow throughout the site for description fields and list elements.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,28 @@
 module PostsHelper
+  def post_list_image(post)
+
+    if post.organization && !post.organization.communities.empty?
+      border = "border: 5px solid #{post.organization.color}"
+    else
+      border = ''
+    end
+
+    if post.image
+      link_to post do
+        image_tag post.image.thumb('75x75#').url, style: border
+      end
+    elsif post.og_image && post.og_image != '//:0' && !post.og_image.blank?
+      link_to post do
+       image_tag post.og_image, style: border
+      end
+    elsif post.start_date
+      link_to post do
+        image_tag 'calendar-icon.svg', size: '75x75', alt: post.title, style: border
+      end
+     else
+      link_to post do
+        image_tag 'bcn_logo_black.svg', size: '75x75', alt: post.title, style: border
+       end
+    end
+  end
 end
